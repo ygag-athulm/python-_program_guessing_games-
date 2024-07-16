a7deb22fdd2a7f5f7557decaa95f598e47b65406
athulm <athularavind1999@gamil.com>
2024-07-16 16:33:37 +0530

    program editted

--- happy_number.py
+++ happy_number.py
@@ -1,9 +1,9 @@
-def  happy_number_game():
-    num = int(input("Enter the digit"))
+def happy_number_game():
+    num = int(input("Enter the digit: "))
     while num >= 10:
         digits = [int(dig) ** 2 for dig in str(num)]
         num = sum(digits)
-    if num ==1:
+    if num == 1:
         return True
     else:
         return False
